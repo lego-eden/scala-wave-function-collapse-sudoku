69b018d920e172915d164c83d67cc96d24f7bb95
lego-eden <hampus.eden@gmail.com>
2024-07-23 19:18:38 +0200

    Replace Int with SudokuCell-enum and add tests

--- SudokuGrid.scala
+++ SudokuGrid.scala
@@ -1,7 +1,11 @@
 import scala.collection.immutable.Set
-import SudokuGrid.SudokuCell
+import scala.annotation.targetName
 
-case class SudokuGrid private (grid: Vector[Vector[SudokuCell]]):
+case class SudokuGrid private (private val grid: Vector[Vector[SudokuGrid.SudokuCell]]):
+  import SudokuGrid.SudokuCell
+
+  require(grid.forall(_.size == 9) && grid.size == 9)
+
   override def toString(): String =
     "SudokuGrid:\n" +
     grid.zipWithIndex.map((rowVec, i) => 
@@ -16,24 +20,30 @@ case class SudokuGrid private (grid: Vector[Vector[SudokuCell]]):
       else rowStr.toString()
     ).mkString("\n")
 
-  def solved(): Option[SudokuGrid] =
-    ???
+  def solved: Option[SudokuGrid] =
+    None
 
   infix def where(loc: ((Int, Int), Int)): SudokuGrid =
     val ((row, col), value) = loc
-    this.copy(grid.updated(row, grid(row).updated(col, SudokuCell(value))))
+    this.copy(grid.updated(row, grid(row).updated(col, SudokuCell.Known(value))))
+
+  infix def where(locs: ((Int, Int), Int)*): SudokuGrid =
+    var SudokuGrid(newGrid) = this
+    locs.foreach(loc =>
+      val ((row, col), value) = loc
+      newGrid = newGrid.updated(row, newGrid(row).updated(col, SudokuCell.Known(value)))
+    )
+    SudokuGrid(newGrid)
 
 object SudokuGrid:
   def apply(): SudokuGrid =
-    new SudokuGrid(Vector.fill(9)(Vector.fill(9)(SudokuCell(0))))
+    new SudokuGrid(Vector.fill(9)(Vector.fill(9)(SudokuCell.Known(0))))
   
-  def apply(grid: Vector[Vector[SudokuCell]]): SudokuGrid =
-    require(grid.forall(_.size == 9) && grid.size == 9)
+  @targetName("gridapply")
+  def apply(grid: Vector[Vector[Int]]): SudokuGrid =
+    new SudokuGrid(grid.map(_.map(SudokuCell.Known(_))))
 
-    new SudokuGrid(grid)
-  
-
-  enum SudokuCell:
+  private enum SudokuCell:
     case Known(value: Int)
     case Unknown(values: Set[Int])
     case Invalid
@@ -50,8 +60,7 @@ object SudokuGrid:
       case Known(value) => value.toString()
       case Unknown(values) => "-"
       case Invalid => "X"
-
-  object SudokuCell:
-    def apply(value: Int): SudokuCell = Known(value)
-    def apply(values: Set[Int]): SudokuCell = Unknown(values)
+  
+  private object SudokuCell:
+    def default(): SudokuCell = Unknown(Set(1 to 9 *))
     
--- SudokuGrid.test.scala
+++ SudokuGrid.test.scala
@@ -3,11 +3,9 @@
 class SudokuTests extends munit.FunSuite:
 
   test("EmptySolve"):
-    assert(SudokuGrid().solved().nonEmpty)
+    assert(SudokuGrid().solved.nonEmpty)
   
   test("ImpossibleSolve"):
     val unsolvableSudoku = SudokuGrid() where ((0, 0) -> 1)
-    assert(SudokuGrid().solved().isEmpty)
-    
-
+    assert(SudokuGrid().solved.isEmpty)
 end SudokuTests
